e8a9e6b9a0e395bf90f6b5217843b1b1681a392f
Brion <info@brionmario.com>
2024-06-08 17:12:46 +0530

    chore(react): add test util

--- packages/aydan-ui-react/test-configs/utils.tsx
+++ packages/aydan-ui-react/test-configs/utils.tsx
@@ -22,5 +22,30 @@
  * SOFTWARE.
  */
 
-import '@testing-library/jest-dom';
-import '@testing-library/jest-dom/jest-globals';
+import {RenderResult, render as rtlRender, RenderOptions} from '@testing-library/react';
+import {ComponentType, PropsWithChildren, ReactElement} from 'react';
+import ThemeProvider from '../src/theme/ThemeProvider';
+
+/**
+ * Custom render method to includes things like global context providers, data stores, etc.
+ * @see {@link https://testing-library.com/docs/react-testing-library/setup#custom-render} for more info.
+ *
+ * @param ui - Component to render.
+ * @param renderOptions - Render options.
+ *
+ * @return {RenderResult}
+ */
+const render = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>): RenderResult => {
+  const Wrapper = (props: PropsWithChildren<ComponentType>): ReactElement => {
+    const {children} = props;
+
+    return <ThemeProvider>{children}</ThemeProvider>;
+  };
+
+  return rtlRender(ui, {wrapper: Wrapper as any, ...options});
+};
+
+// re-export everything
+export * from '@testing-library/react';
+// override render method
+export {render};
