1d39f91da418cb55f2b9461e795b1df8537d9803
Brion <info@brionmario.com>
2024-12-30 17:35:54 +0530

    feat(react): add `Badge` component

--- packages/aydan-ui-react/src/Badge/Badge.tsx
+++ packages/aydan-ui-react/src/Badge/Badge.tsx
@@ -22,18 +22,32 @@
  * SOFTWARE.
  */
 
-// FILE LEVEL ESLINT SUPPRESSIONS:
-// This is the Public API and should have both named and default exports.
-/* eslint-disable import/prefer-default-export */
+import * as React from 'react';
+import {cva, type VariantProps} from 'class-variance-authority';
 
-export * from './Accordion/Accordion';
-export * from './Alert/Alert';
-export * from './AlertDialog/AlertDialog';
-export * from './AspectRatio/AspectRatio';
-export * from './Avatar/Avatar';
-export * from './Badge/Badge';
-export * from './Button/Button';
+import {cn} from '@aydan-ui/utils';
 
-export {default as ThemeContext} from './styles/ThemeContext';
-export {default as ThemeProvider} from './styles/ThemeProvider';
-export {default as useTheme} from './styles/use-theme';
+const badgeVariants = cva(
+  'inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+  {
+    variants: {
+      variant: {
+        default: 'border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80',
+        secondary: 'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
+        destructive: 'border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80',
+        outline: 'text-foreground',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  },
+);
+
+export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof badgeVariants> {}
+
+const Badge = ({className, variant, ...props}: BadgeProps) => (
+  <div className={cn(badgeVariants({variant}), className)} {...props} />
+);
+
+export {Badge, badgeVariants};
