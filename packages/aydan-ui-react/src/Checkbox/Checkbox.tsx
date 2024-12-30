82fd95fc884ac1b44588b3a4373d901d241ff881
Brion <info@brionmario.com>
2024-12-30 19:51:05 +0530

    feat(react): add `Checkbox` component

--- packages/aydan-ui-react/src/Checkbox/Checkbox.tsx
+++ packages/aydan-ui-react/src/Checkbox/Checkbox.tsx
@@ -22,24 +22,31 @@
  * SOFTWARE.
  */
 
-// FILE LEVEL ESLINT SUPPRESSIONS:
-// This is the Public API and should have both named and default exports.
-/* eslint-disable import/prefer-default-export */
+'use client';
 
-export * from './Accordion/Accordion';
-export * from './Alert/Alert';
-export * from './AlertDialog/AlertDialog';
-export * from './AspectRatio/AspectRatio';
-export * from './Avatar/Avatar';
-export * from './Badge/Badge';
-export * from './Breadcrumb/Breadcrumb';
-export * from './Button/Button';
-export * from './Calendar/Calendar';
-export * from './Card/Card';
-export * from './Carousel/Carousel';
-export * from './Chart/Chart';
-export * from './Checkbox/Checkbox';
+import * as React from 'react';
+import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
+import {Check} from 'lucide-react';
 
-export {default as ThemeContext} from './styles/ThemeContext';
-export {default as ThemeProvider} from './styles/ThemeProvider';
-export {default as useTheme} from './styles/use-theme';
+import {cn} from '@aydan-ui/utils';
+
+const Checkbox = React.forwardRef<
+  React.ElementRef<typeof CheckboxPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
+>(({className, ...props}, ref) => (
+  <CheckboxPrimitive.Root
+    ref={ref}
+    className={cn(
+      'peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
+      className,
+    )}
+    {...props}
+  >
+    <CheckboxPrimitive.Indicator className={cn('flex items-center justify-center text-current')}>
+      <Check className="h-4 w-4" />
+    </CheckboxPrimitive.Indicator>
+  </CheckboxPrimitive.Root>
+));
+Checkbox.displayName = CheckboxPrimitive.Root.displayName;
+
+export {Checkbox};
