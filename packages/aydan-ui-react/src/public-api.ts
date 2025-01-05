620d1b7ad2f4916a561c762706a8c1d93f6db961
Brion <info@brionmario.com>
2025-01-05 14:27:43 +0530

    feat(react): add `Command` component

--- packages/aydan-ui-react/src/public-api.ts
+++ packages/aydan-ui-react/src/public-api.ts
@@ -39,6 +39,7 @@ export * from './Card/Card';
 export * from './Carousel/Carousel';
 export * from './Chart/Chart';
 export * from './Checkbox/Checkbox';
+export * from './Command/Command';
 export * from './Dialog/Dialog';
 
 export {default as ThemeContext} from './styles/ThemeContext';
