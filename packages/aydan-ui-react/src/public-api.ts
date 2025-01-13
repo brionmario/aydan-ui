7c89789adccc63cf3e95576b0271c46aa753f846
Brion <info@brionmario.com>
2025-01-13 17:21:40 +0530

    feat(react): add `Select` component

--- packages/aydan-ui-react/src/public-api.ts
+++ packages/aydan-ui-react/src/public-api.ts
@@ -56,6 +56,7 @@ export * from './Progress/Progress';
 export * from './RadioGroup/RadioGroup';
 export * from './Resizable/Resizable';
 export * from './ScrollArea/ScrollArea';
+export * from './Select/Select';
 
 export {default as ThemeContext} from './styles/ThemeContext';
 export {default as ThemeProvider} from './styles/ThemeProvider';
