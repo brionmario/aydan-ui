db6755141f10212e2d7ed6e0a5889cbab10a924f
Brion <info@brionmario.com>
2025-01-14 18:56:33 +0530

    feat(react): add `Tabs` component

--- packages/aydan-ui-react/src/public-api.ts
+++ packages/aydan-ui-react/src/public-api.ts
@@ -64,6 +64,7 @@ export * from './Skeleton/Skeleton';
 export * from './Slider/Slider';
 export * from './Switch/Switch';
 export * from './Table/Table';
+export * from './Tabs/Tabs';
 export * from './Tooltip/Tooltip';
 
 export * from './useMobile/useMobile';
