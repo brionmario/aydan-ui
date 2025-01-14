40ec07b45042a2b5a6ce3cb7cf59a7ad7f650e8a
Brion <info@brionmario.com>
2025-01-14 18:02:45 +0530

    feat(react): add `Skeleton` component

--- packages/aydan-ui-react/src/public-api.ts
+++ packages/aydan-ui-react/src/public-api.ts
@@ -59,6 +59,7 @@ export * from './ScrollArea/ScrollArea';
 export * from './Select/Select';
 export * from './Separator/Separator';
 export * from './Sheet/Sheet';
+export * from './Skeleton/Skeleton';
 export * from './Tooltip/Tooltip';
 
 export * from './useMobile/useMobile';
